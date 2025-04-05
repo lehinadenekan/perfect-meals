4ce6c6a5f950566b7ec5f77c07d7c2468dd14b77
Timilehin Adenekan <lehinadenekan@gmail.com>
2025-04-05 11:04:03 +0100

    feat: add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './globals.css'
 import { Providers } from './providers'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   )
