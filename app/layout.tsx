18583514eea433e2c16a37f39fa218bc59f1838c
Timilehin Adenekan <lehinadenekan@gmail.com>
2025-04-05 20:07:52 +0100

    feat: Add Google AdSense script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1744468975179679"
+             crossorigin="anonymous"></script>
+      </head>
       <body>
         <Providers>{children}</Providers>
         <Analytics />
