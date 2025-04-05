8f957ba970f3cae5bc2815b29c39a99e6a483505
Timilehin Adenekan <lehinadenekan@gmail.com>
2025-04-05 19:59:12 +0100

    feat: Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './globals.css'
 import { Providers } from './providers'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -13,6 +14,7 @@ export default function RootLayout({
       <body>
         <Providers>{children}</Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
